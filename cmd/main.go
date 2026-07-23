e333bccf92008f26721d4a7e811773b481d24a60
Gleb Kyshtymov <glebkyshtymovtablet@google.com>
2026-07-23 20:19:39 +0300

    feat: initiating the game

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main{
+	initGame()
+}
+
+func initGame() {
+	doorClosed := true
+	isBackpackOn := false
+	itemMap := map[string]bool{"ключи": false, "конспекты": false}
+
+	currentPlace = "кухня"
+}
